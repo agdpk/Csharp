f19af6a007acfc02456712e7fdfdaefb03b07915
Anastasia <gris.anastasyia@gmail.com>
2023-11-18 06:53:33 +0300

    64 - 68

---
+++ task66сумма_чисел_рекурсия/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+System.Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int intN(int numberM, int numberN)
+{
+    int sum = 0;
+    if (numberM <= numberN) sum += numberM + intN(numberM + 1, numberN);
+
+    return sum;
+}
+
+int sum = intN(numberM, numberN);
+System.Console.WriteLine($"Сумма натуральных элементов: {sum}");
---
+++ task68функция_Аккермана/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+System.Console.WriteLine("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int A(int m, int n)
+{
+    if (m == 0)         return n + 1;
+    else if (n == 0)    return A(m - 1, 1);
+    else                return A(m - 1, A(m, n - 1));
+}
+
+
+int result = A(numberM, numberN);
+System.Console.WriteLine(result);
